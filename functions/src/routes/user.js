fc09172b12d1769e436a77d942d47d6b87bc8d13
Ed Adkins <ejmadkins@gmail.com>
2022-07-06 18:44:36 +0100

    add initial routes for users endpoint

--- functions/src/routes/user.js
+++ functions/src/routes/user.js
@@ -1,1 +1,8 @@
-// placeholder
+const userController = require('../controllers/user');
+const express = require('express');
+const router = express.Router();
+
+router.post('/', userController.create);
+router.get('/', userController.read);
+
+module.exports = router;
