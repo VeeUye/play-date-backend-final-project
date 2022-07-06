28b67f686d36c7a726089c57b0159348dd5c5e47
Ed Adkins <ejmadkins@gmail.com>
2022-07-06 18:44:09 +0100

    add initial controllers for users endpoint

--- functions/src/controllers/user.js
+++ functions/src/controllers/user.js
@@ -1,1 +1,23 @@
-// placeholder
+'use strict';
+
+const { db } = require('../db');
+
+exports.create = async (req, res) => {
+  try {
+    const data = req.body;
+    await db.collection('user').doc().set(data);
+    res.send('Record saved successfuly');
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
+exports.read = async (_, res) => {
+  try {
+    const snapshot = await db.collection('user').get();
+    const userArray = snapshot.docs.map((doc) => doc.data());
+    res.send(userArray);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
