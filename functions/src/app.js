d10eab93cabf36516b3813fb7c8af99ff1988057
Ed Adkins <ejmadkins@gmail.com>
2022-07-11 09:50:44 +0100

    add support for cors

--- functions/src/app.js
+++ functions/src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const userRouter = require('./routes/user');
 const eventRouter = require('./routes/event');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 module.exports = app;
 
