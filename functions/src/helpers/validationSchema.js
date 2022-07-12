113e069f2b0dced4ec7c57a7326e6c285d9efbfd
Paul Beard <pbeardey@gmail.com>
2022-07-12 23:43:34 +0100

    replace friends_rejected with friends_declined

--- functions/src/helpers/validationSchema.js
+++ functions/src/helpers/validationSchema.js
@@ -5,10 +5,10 @@ const eventSchema = Joi.object({
   date_end: Joi.date().greater(Joi.ref('date_start')).required(),
   friends_invited: Joi.array().items(Joi.string()).required(),
   friends_accepted: Joi.array().items(Joi.string()).default([]),
-  friends_rejected: Joi.array().items(Joi.string()).default([]),
-  location: Joi.string(),
-  description: Joi.string(),
-  owner: Joi.string()
+  friends_declined: Joi.array().items(Joi.string()).default([]),
+  location: Joi.string().required(),
+  description: Joi.string().required(),
+  owner: Joi.string().required(),
 })
 
 module.exports = { eventSchema };
