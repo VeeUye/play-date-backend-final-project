861c9eb69d9298f7971d1abf2282910d60d7674f
Paul Beard <pbeardey@gmail.com>
2022-07-13 17:07:10 +0100

    change friends-confirmed to friends-accepted

--- functions/src/__tests__/dummy-data.js
+++ functions/src/__tests__/dummy-data.js
@@ -3,7 +3,7 @@ const DUMMY_DATA = [
     date_start : "Sat Sep 10 2022 11:30:00 GMT+0100",
     date_end : "Sat Sep 10 2022 13:30:00 GMT+0100",
     friends_invited : ["userId2", "userId3", "userId4"],
-    friends_confirmed : ["userId2", "userId3"],
+    friends_accepted : ["userId2", "userId3"],
     location : "Manchester",
     description : "Event1",
     owner : "userId1"
@@ -12,7 +12,7 @@ const DUMMY_DATA = [
     date_start : "Thu Aug 11 2022 17:30:00 GMT+0100",
     date_end : "Thu Aug 11 2022 17:30:00 GMT+0100",
     friends_invited : ["userId3", "userId4", "userId5"],
-    friends_confirmed : ["userId4"],
+    friends_accepted : ["userId4"],
     location : "Manchester",
     description : "Event2",
     owner : "userId2"
@@ -21,7 +21,7 @@ const DUMMY_DATA = [
     date_start : "Sat Aug 13 2022 09:15:00 GMT+0100",
     date_end : "Sat Aug 13 2022 11:45:00 GMT+0100",
     friends_invited : ["userId1", "userId3", "userId5"],
-    friends_confirmed : ["userId1"],
+    friends_accepted : ["userId1"],
     location : "Liverpool",
     description : "Event3",
     owner : "userId1"
@@ -30,7 +30,7 @@ const DUMMY_DATA = [
     date_start : "Tue Aug 09 2022 09:15:00 GMT+0100",
     date_end : "Tue Aug 09 2022 13:30:00 GMT+0100",
     friends_invited : ["userId6", "userId2", "userId5"],
-    friends_confirmed : ["userId2"],
+    friends_accepted : ["userId2"],
     location : "Leeds",
     description : "Event4",
     owner : "userId4"
