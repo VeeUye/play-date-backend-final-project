cdb536c25c73a824fef370f3f2f48faddd0b24e9
Paul Beard <pbeardey@gmail.com>
2022-07-13 17:08:29 +0100

    remove required from friends_invited for now

--- functions/src/helpers/validationSchema.js
+++ functions/src/helpers/validationSchema.js
@@ -3,7 +3,7 @@ const Joi = require ('joi');
 const eventSchema = Joi.object({
   date_start: Joi.date().greater('now').required(),
   date_end: Joi.date().greater(Joi.ref('date_start')).required(),
-  friends_invited: Joi.array().items(Joi.string()).required(),
+  friends_invited: Joi.array().items(Joi.string()),
   friends_accepted: Joi.array().items(Joi.string()).default([]),
   friends_declined: Joi.array().items(Joi.string()).default([]),
   location: Joi.string().required(),
