f8bf315d2916c6078412e75fe9d8cf218f4c8241
Ed Adkins <ejmadkins@gmail.com>
2022-07-14 12:41:44 +0100

    add firebase authorization middleware

--- functions/src/app.js
+++ functions/src/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const userRouter = require('./routes/user');
 const eventRouter = require('./routes/event');
+const validateFirebaseIdToken = require('./middlewares/authTokenValidation');
 
 const app = express();
 app.use(cors());
+app.use(validateFirebaseIdToken);
 app.use(express.json());
 module.exports = app;
 
