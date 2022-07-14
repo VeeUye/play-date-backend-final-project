85f158da88333cb781d9449c550250a92c109993
Paul Beard <pbeardey@gmail.com>
2022-07-14 11:01:05 +0100

    update test to test pending/accepted/declined

--- functions/src/__tests__/events.test.js
+++ functions/src/__tests__/events.test.js
@@ -11,7 +11,7 @@ const JSDOtoTimestamp = (d) => {
  return dbDate;
 }
 
-describe('events testing', () => {
+describe('events get testing', () => {
   
   before('test data into db', async () => {
     let data = DUMMY_DATA;
@@ -36,57 +36,86 @@ describe('events testing', () => {
       expect(events).to.have.lengthOf(4);
       expect(events[0].id).to.equal('Event1');
     });
+
+    it('GET events user has accepted', async () => {
+      const userId = 'userId1';
+      const res = await request(app)
+      .get(`/events/user-events/${userId}`);
+
+      const events = JSON.parse(res.text);
+      expect(events).to.have.lengthOf(2);
+      expect(events[0].description).to.equal('Event3');
+      expect(events[1].description).to.equal('Event1');
+      })
+
+    it('GET pending events for user', async () =>{
+      const userId = 'userId1';
+      const res = await request(app)
+      .get(`/events/user-events/${userId}/pending`)
+
+      const events = JSON.parse(res.text);
+      expect(events).to.have.lengthOf(1);
+      expect(events[0].description).to.equal('Event2');
+    });
+
+    it('GET events user has declined', async () =>{
+      const userId = 'userId1';
+      const res = await request(app)
+      .get(`/events/user-events/${userId}/declined`)
+
+      const events = JSON.parse(res.text);
+      expect(events).to.have.lengthOf(1);
+      expect(events[0].description).to.equal('Event4');
+    });
   });
   
-  
-  describe('POST events tests', () => {
+});
+
+describe('POST events tests', () => {
    
-    const event5 = {
-      date_start : "Sat Dec 10 2022 11:30:00 GMT+0100",
-      date_end : "Sat Dec 10 2022 13:30:00 GMT+0100",
-      friends_invited : ["userId2", "userId3", "userId4"],
-      friends_accepted : ["userId2", "userId3"],
-      location : "Lancaster",
-      description : "Event5",
-      owner : "userId4"
-    }
-    
-    it('POST a new event into the db', async () => {
-      const res = await request(app)
-      .post('/events')
-      .send(event5);
-    
-      expect(res.status).to.equal(201);
-      expect(res.text).to.equal('Record saved successfuly');
-    });
-    
+  const event5 = {
+    name : "Event number 5",
+    date_start : "Sat Dec 10 2022 11:30:00 GMT+0100",
+    date_end : "Sat Dec 10 2022 13:30:00 GMT+0100",
+    friends_invited : ["userId2", "userId3", "userId4"],
+    location : "Lancaster",
+    description : "Event5",
+    owner : "userId4"
+  }
+  
+  it('POST a new event into the db', async () => {
+    const res = await request(app)
+    .post('/events')
+    .send(event5);
+  
+    expect(res.status).to.equal(201);
+    expect(res.text).to.equal('Record saved successfuly');
   });
-  
-  
-  describe('accept/decline events', () => {
-  
-    it('PUT userId4 to accept invite', async () => {
-      const res = await request(app)
-      .put('/events/user-events/accept')
-      .send({"eventId":"Event3","userId":"userId3"});
-    
-      expect(res.status).to.equal(200);
-      expect(res.text).to.equal('Success - invite accepted');
-    });
-    
-    it('PUT userId5 to decline invite', async () => {
-      const res = await request(app)
-      .put('/events/user-events/decline')
-      .send({"eventId":"Event3","userId":"userId5"});
-    
-      expect(res.status).to.equal(200);
-      expect(res.text).to.equal('Success - invite declined');
-    });
-
-  });
-
 
 });
+
+  
+describe('accept/decline events', () => {
+
+  it('PUT userId4 to accept invite', async () => {
+    const res = await request(app)
+    .put('/events/user-events/accept')
+    .send({"eventId":"Event3","userId":"userId3"});
+  
+    expect(res.status).to.equal(200);
+    expect(res.text).to.equal('Success - invite accepted');
+  });
+  
+  it('PUT userId5 to decline invite', async () => {
+    const res = await request(app)
+    .put('/events/user-events/decline')
+    .send({"eventId":"Event3","userId":"userId5"});
+  
+    expect(res.status).to.equal(200);
+    expect(res.text).to.equal('Success - invite declined');
+  });
+});
+
 
 
 
