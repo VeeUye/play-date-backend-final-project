0676185e4b64b34de42eb5980a4c08258ee43e44
Paul Beard <pbeardey@gmail.com>
2022-07-16 15:49:27 +0100

    tidy up userSchema

--- functions/src/models/userSchema.js
+++ functions/src/models/userSchema.js
@@ -2,14 +2,14 @@ const Joi = require ('joi');
  
 const createUserSchema = Joi.object({
  // userId: Joi.string().required,
- // name: Joi.string().required(),
- // email: Joi.string().email().required,
  userId: Joi.string(),
+ // name: Joi.string().required(),
  name: Joi.string(),
+ // email: Joi.string().email().required,
  email: Joi.string().email(),
  description: Joi.string(),
  location: Joi.string(),
- // friends: Joi.array().items(Joi.string()).default([]),
+ friends: Joi.array().items(Joi.string()),
  // children: Joi.array().items(Joi.string()).required,
  children: Joi.string(),
  profile_pic: Joi.string().dataUri(),
@@ -21,6 +21,7 @@ const editUserSchema = Joi.object({
  description: Joi.string(),
  location: Joi.string(),
  friends: Joi.array().items(Joi.string()),
+  // children: Joi.array().items(Joi.string()).required,
  children: Joi.string(),
  profile_pic: Joi.string().dataUri(),
 })
